a60b081d970996c13b2cbc0a8e2832c69aaf0429
leahbalter <22poun@users.noreply.github.com>
2018-06-24 13:28:49 -0700

    removed general processing from the senate script to its own script

---
+++ General Code/initProcessing.R
@@ -0,0 +1,21 @@
+transFileName <- readline(prompt = "Please enter the name of the trans .txt file")
+year <- readline(prompt = "Please enter the year")
+
+trans <- read.delim(transFileName, header = FALSE, sep = '|')
+trans <- trans[, c(1, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16)]
+names(trans) <- c("sendID", "elType", "transCode", "recType", "recName", "recCity", "recState", "recZIP", "date", "amount", "recID")
+trans <- subset(trans, transCode == "24K" | transCode == "24Z" | transCode == "24R" | transCode == "24E" | transCode == "24C" | transCode == "24H" | transCode == "24F")
+
+save(trans, file = paste('rawTrans', year, '.rda', sep = "", collapse = NULL))
+
+cn <- read.delim(paste('cn', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
+cn <- cn[, c(1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 14, 15)]
+names(cn) = c("canID", "canName", "party", "elYear", "canState", "office", "district", "incumbent", "pccID", "cityAdd", "stateAdd", "zipAdd")
+
+save(cn, file = paste('cn', year, '.rda', sep = "", collapse = NULL))
+
+
+com <- read.delim(paste('cm', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
+com <- com[, c(1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)]
+names(com) = c("comID", "comName", "treasName", "city", "state", "zip", "comDes", "comType", "comParty", "filingFreq", "interestGroup", "connOrg", "canID")
+save(cm, file = paste('cm', year, '.rda', sep = "", collapse = NULL))
--- General Code/senGenProc.R
+++ General Code/senGenProc.R
@@ -1,21 +1,9 @@
-transFileName <- readline(prompt = "Please enter the name of the trans .txt file")
 year <- readline(prompt = "Please enter the year")
 
-trans <- read.delim(transFileName, header = FALSE, sep = '|')
-trans <- trans[, c(1, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16)]
-names(trans) <- c("sendID", "elType", "transCode", "recType", "recName", "recCity", "recState", "recZIP", "date", "amount", "recID")
-trans <- subset(trans, transCode == "24K" | transCode == "24Z" | transCode == "24R" | transCode == "24E" | transCode == "24C" | transCode == "24H" | transCode == "24F")
-
-save(trans, file = paste('rawTrans', year, '.rda', sep = "", collapse = NULL))
-
-cn <- read.delim(paste('cn', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
-cn <- cn[, c(1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 14, 15)]
-names(cn) = c("canID", "canName", "party", "elYear", "canState", "office", "district", "incumbent", "pccID", "cityAdd", "stateAdd", "zipAdd")
-
-save(cn, file = paste('cn', year, '.rda', sep = "", collapse = NULL))
+load(file = paste('cn', year, '.rda', sep = "", collapse = NULL))
+load(file = paste('rawTrans', year, '.rda', sep = "", collapse = NULL))
 
 sen <- subset(cn, office == "S")
-
 state <- data.frame("state", stringsAsFactors = FALSE)
 names(state) = c("state")
 
@@ -41,7 +29,7 @@ for (i in 1:nrow(senGenTrans)) {
 
 senGenTrans$state = NA
 senGenTrans$state = state
-save(senGenTrans, file = paste('senGenTrans', year, '.rda', sep = "", collapse = NULL))
+save(senGenTrans, paste('senGenTrans', year, '.rda', sep = "", collapse = NULL))
 
 temp <- data.frame(senGenTrans$sendID, senGenTrans$amount, senGenTrans$state)
 names(temp) = c("sendID", "amount", "state")
