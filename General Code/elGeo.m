87eadee8ccf91ca8b615741878ade93535fd1df6
leahbalter <leahbalter@users.noreply.github.com>
2018-07-09 20:55:16 -0700

    automate gitfiles

--- General Code/elGeo.m
+++ General Code/elGeo.m
@@ -3,11 +3,13 @@
 % THEN compCom.m
 
 % elGeo.m and commGeo.m should be run *once* on each network
-% commGeo.m finds the community identifiers, which may change if you run it
-% again as such, once you decide what community identifiers you will be 
-% working with run *all* comparisons for communities using the same 
-% identifiers i.e. don't rerun commGeo.m between running compCom.m on 
-% different nodes because you'll get different results
+% commGeo.m finds the commuity identifiers, which may change if you run it
+% again
+% as such, once you decide what community identifiers you will be working
+% with
+% run *all* comparisons for communities using the same identifiers
+% ie don't rerun compGeo.m between running compCom on different nodes
+% because you'll get different results
 
 % this is specifically for the geographic networks
 % you need to have the states.mat file in the same directory for this
@@ -44,6 +46,8 @@ fileName = input('Please enter the .txt edgelist file ', 's')
 year = input('Please enter the year ');
 year = num2str(year);
 
+gitFileName = input('Please enter the git file location: ', 's')
+
 tdfread(fileName, ' ');
 
 el = [sendID state amount];
@@ -51,7 +55,6 @@ sendID = cellstr(sendID);
 state = cellstr(state);
 
 g = graph(sendID, state, amount);
-a = adjacency(g);
 weight = g.Edges.Weight;
 
 deg = centrality(g, 'degree', 'Importance', weight);
@@ -74,10 +77,11 @@ rowNames = table2array(g.Nodes(:,1));
 ind = find(ismember(rowNames, states)); % and this is an index of all rows that have state value
 gState = g.Nodes(ind,:);
 
-save(strcat('../Data and Results/2006/gGeo', year, '.mat'), 'g');
-save(strcat('../Data and Results/2006/weight', year, '.mat'), 'weight');
-save(strcat('../Data and Results/2006/states', year, '.mat'), 'gState');
+save(strcat('gGeo', year, '.mat'), 'g');
+save(strcat('states', year, '.mat'), 'gState');
 
-writetable(g.Nodes, strcat('../Data and Results/2006/nodes', year, '.csv'));
-writetable(gState, strcat('../Data and Results/2006/stateNodes', year, '.csv'));
+writetable(g.Nodes, strcat('nodes', year, '.csv'));
+writetable(gState, strcat('stateNodes', year, '.csv'));
+writetable(g.Nodes, strcat(gitFileName, '\nodes', year, '.csv'))
+writetable(gState, strcat(gitFileName, '\stateNodes', year, '.csv'))
 clear
