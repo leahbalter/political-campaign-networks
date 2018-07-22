32f62b8ce384a7fa0e280b3abbbd997f6d22230e
leahbalter <leahbalter@users.noreply.github.com>
2018-07-22 11:11:00 -0700

    includes 24A
    superPAC money

--- General Code/initProcessing.R
+++ General Code/initProcessing.R
@@ -8,19 +8,19 @@ rm(list = 'rawTrans')
 
 trans <- trans[, c(1, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16)]
 names(trans) <- c("sendID", "elType", "transCode", "recType", "recName", "recCity", "recState", "recZIP", "date", "amount", "recID")
-trans <- subset(trans, transCode == '24A' | transCode == "24K" | transCode == "24Z" | transCode == "24R" | transCode == "24E" | transCode == "24C" | transCode == "24H" | transCode == "24F")
+trans <- subset(trans, transCode == '24N' | transCode == '24A' | transCode == "24K" | transCode == "24Z" | transCode == "24R" | transCode == "24E" | transCode == "24C" | transCode == "24H" | transCode == "24F")
 
 trans$date <- as.Date(sprintf('%08d', trans$date), '%m%d%Y')
 
 save(trans, file = paste('trans', year, '.rda', sep = "", collapse = NULL))
 
-cn <- read.delim(paste('cn', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
-cn <- cn[, c(1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 14, 15)]
-names(cn) = c("canID", "canName", "party", "elYear", "canState", "office", "district", "incumbent", "pccID", "cityAdd", "stateAdd", "zipAdd")
+#cn <- read.delim(paste('cn', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
+#cn <- cn[, c(1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 14, 15)]
+#names(cn) = c("canID", "canName", "party", "elYear", "canState", "office", "district", "incumbent", "pccID", "cityAdd", "stateAdd", "zipAdd")
 
-save(cn, file = paste('cn', year, '.rda', sep = "", collapse = NULL))
+#save(cn, file = paste('cn', year, '.rda', sep = "", collapse = NULL))
 
-com <- read.delim(paste('cm', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
-com <- com[, c(1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)]
-names(com) = c("comID", "comName", "treasName", "city", "state", "zip", "comDes", "comType", "comParty", "filingFreq", "interestGroup", "connOrg", "canID")
-save(com, file = paste('cm', year, '.rda', sep = "", collapse = NULL))
+#com <- read.delim(paste('cm', year, '.txt', sep = "", collapse = NULL), header = FALSE, sep = '|', quote = "")
+#com <- com[, c(1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)]
+#names(com) = c("comID", "comName", "treasName", "city", "state", "zip", "comDes", "comType", "comParty", "filingFreq", "interestGroup", "connOrg", "canID")
+#save(com, file = paste('cm', year, '.rda', sep = "", collapse = NULL))
